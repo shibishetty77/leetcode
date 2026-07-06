7bc76f2e8f8e51d1f36e27659d9368118a7ba539
Shibi R Shetty <shibishetty2@gmail.com>
2026-07-06 20:24:30 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.86%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==1){
+            return true;
+        }
+        if(n<=0){
+            return false;
+        }
+        if(n%2==0){
+            int nnew=n/2;
+            return isPowerOfTwo(nnew);
+        }
+        return false;
+    }
+};
