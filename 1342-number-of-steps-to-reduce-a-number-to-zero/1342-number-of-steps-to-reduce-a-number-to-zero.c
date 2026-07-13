62410e2246a08f9db05bbe494e2d275ac3c90650
Shibi R Shetty <shibishetty2@gmail.com>
2026-07-13 23:07:39 +0530

    Time: 0 ms (100%), Space: 8.7 MB (28.31%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.c
@@ -0,0 +1,13 @@
+int numberOfSteps(int num) {
+    int s=0;
+    while(num!=0){
+    if(num%2==0){
+        num=num/2;
+         s++;
+    }else{
+         num=num-1;
+         s++;
+    }
+    }
+return s;
+}
