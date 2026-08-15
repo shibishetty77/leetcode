4af4c9fcdf94a5d0eee80c8843bf60a7265dd92f
Shibi R Shetty <shibishetty2@gmail.com>
2026-08-15 23:24:58 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.21%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        long long num=n;
+        if(num<0){
+            x=1/x;
+            num=-num;
+        }
+        if(num==0) return 1;
+        double half=myPow(x,num/2);
+        if(num%2==0){
+            return half*half;
+        }
+        return x*half*half;
     }
 };
